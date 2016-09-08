5276f0426b941ccc7403cc2b9dcbac5428cfcd41
SaXXuM <saxxum@gmail.com>
2016-09-08 08:57:39 +0700

    Added Handlebars support.

--- meadowlark.js
+++ meadowlark.js
@@ -2,21 +2,34 @@ var express = require('express');
 
 var app = express();
 
+// set up Handlebars
+var handlebars = require('express-handlebars')
+    .create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
 
 // custom page 404
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Не найдено');
+    res.render('404');
 });
 
 // custom page 500
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Ошибка сервера');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
