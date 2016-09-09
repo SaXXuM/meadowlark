82e94de5c864cdd003e9e86e379c0bce2eebd25a
SaXXuM <saxxum@gmail.com>
2016-09-09 08:59:45 +0700

    Added 'fortune cookie' functionality. Fixed error in layout

--- meadowlark.js
+++ meadowlark.js
@@ -1,23 +1,24 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js')
 
 var app = express();
 
-// set up Handlebars
-var handlebars = require('express-handlebars')
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
     .create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    res.render('about');
+    res.render('about', {fortune: fortune.getFortune()});
 });
-
 
 // custom page 404
 app.use(function(req, res) {
