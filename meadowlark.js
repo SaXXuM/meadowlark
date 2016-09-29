53f12acbbbee2a27f8eee868fe84fe7e5fb9f92b
SaXXuM <saxxum@gmail.com>
2016-09-29 09:03:45 +0700

    Added weather forecast on homepage and form to subscribe on newsletters

--- meadowlark.js
+++ meadowlark.js
@@ -5,16 +5,60 @@ var app = express();
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
-        .create({defaultLayout: 'main'});
+        .create({
+            defaultLayout: 'main',
+            helpers: {
+                section: function (name, options) {
+                    if (!this._sections) this._sections = {};
+                    this._sections[name] = options.fn(this);
+                    return null;
+                }
+            }
+        });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser').urlencoded({extended: true}));
 
 app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
+    next();
+});
+
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: 'Портленд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Сплошная облачность',
+                temp: '54.1 F (12.3 C)'
+            },
+            {
+                name: 'Бенд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Малооблачно',
+                temp: '55.0 F (12.8 C)'
+            },
+            {
+                name: 'Мазанита',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Небольшой дождь',
+                temp: '55.0 F (12.8 C)'
+            }
+        ]
+    }
+}
+
+app.use(function (req, res, next) {
+    if (!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
     next();
 });
 
@@ -27,6 +71,18 @@ app.get('/about', function (req, res) {
         fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
     });
+});
+
+app.get('/newsletter', function (req, res) {
+    res.render('newsletter', {csrf: 'CSRF token goes here'})
+});
+
+app.post('/process', function (req, res) {
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
 });
 
 app.get('/tours/hood-river', function (req, res) {
