004eddd32d0f2b05dcd123ed44c21359c88b7b3a
SaXXuM <saxxum@gmail.com>
2016-10-13 09:03:40 +0700

    Added email templates

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
+var jqupload = require('jquery-file-upload-middleware');
+var nodemailer = require('nodemailer');
+var credentials = require('./credentials.js')
 
+
+var mailTransport = nodemailer.createTransport('SMTP', {
+    service: 'Gmail',
+    auth: {
+        user: credentials.gmail.user,
+        pass: credentials.gmail.pass
+    }
+})
 var app = express();
 
 // set up handlebars view engine
@@ -26,6 +38,32 @@ app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
     next();
+});
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret
+}));
+
+app.use(function (req, res, next) {
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
+
+app.use('/upload', function (req, res, next) {
+    var now = Date.now();
+    console.log('Uploading file');
+    jqupload.fileHandler({
+        uploadDir: function () {
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function () {
+            return '/uploads/' + now;
+        }
+    })(req, res, next);
 });
 
 function getWeatherData() {
@@ -66,12 +104,71 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/contest/vacation-photo', function (req, res) {
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year: now.getFullYear(), 
+        month: now.getMonth()
+    });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        if(err) return res.redirect(303, '/error');
+        console.log('recieved fields:');
+        console.log(fields);
+        console.log('recieved files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
+
 app.get('/about', function (req, res) {
     res.render('about', {
         fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
     });
 });
+
+function NewsletterSignup() {
+}
+
+NewsletterSignup.prototype.save = function(cb){
+    cb();
+};
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function (req, res) {
+    var name = req.body.name || '', email = req.body.email || '';
+    if(!email.match(VALID_EMAIL_REGEX)) {
+        if(req.xhr) return res.json({error: 'Некорректынй адрес электронной почты.'});
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Ошибка проверки!',
+            message: 'Введенный вами адрес электронной почты некорректен.'
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+    new NewsletterSignup({ name: name, email: email }).save(function(err){
+        if(err) {
+            if(req.xhr) return res.json({ error: 'Database error.' });
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try again later.',
+            };
+            return res.redirect(303, '/newsletter/archive');
+        }
+        if(req.xhr) return res.json({ success: true });
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
+})
 
 app.get('/newsletter', function (req, res) {
     res.render('newsletter', {csrf: 'CSRF token goes here'})
@@ -92,6 +189,10 @@ app.get('/tours/hood-river', function (req, res) {
 app.get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
 });
+
+app.get('/thank-you', function (req, res) {
+    res.render('thank-you');
+})
 
 // custom page 404
 app.use(function(req, res) {
